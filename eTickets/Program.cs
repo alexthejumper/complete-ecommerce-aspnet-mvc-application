5cdfbd66160a82eb202b6a50ae4f28c47a35d1d1
Alexandre Chloe <132390197+alexthejumper@users.noreply.github.com>
2023-06-26 23:20:14 +0400

    Update Program.cs

--- eTickets/Program.cs
+++ eTickets/Program.cs
@@ -1,7 +1,6 @@
 using eTickets.Data;
 using Microsoft.EntityFrameworkCore;
 
-var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
